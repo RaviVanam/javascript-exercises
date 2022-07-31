136ea5d59790140ec5f6df2f96a7edba0a9cb0b8
Ravi Vanam <ravivanam9922@gmail.com>
2022-07-31 16:59:09 +0530

    solve 5, 11, 12, 13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    let oldestPerson;
+    let oldestPersonAge = 0;
 
+    const todayDateTime = new Date();
+    let currentPersonAge;
+    for (const person of persons) {
+        if (person.yearOfDeath) {
+            currentPersonAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            currentPersonAge = todayDateTime.getFullYear() - person.yearOfBirth;
+        }
+        
+        if (oldestPersonAge < currentPersonAge) {
+            oldestPerson = person;
+            oldestPersonAge = currentPersonAge;
+        }
+    } 
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,27 @@
-const caesar = function() {
+const caesar = function(phrase, shift) {
+    while (shift < 0) {
+        shift = shift + 26;
+    }
 
+    let cipher = '';
+    let charcode;
+    for (let i = 0; i < phrase.length; i++) {
+        if (phrase[i] <= 'z' && phrase[i] >= 'a' ) {
+            charcode = phrase[i].charCodeAt() + shift;
+            while (charcode > 'z'.charCodeAt()) {
+                charcode -= 26;
+            } 
+        } else if (phrase[i] <= 'Z' && phrase[i] >= 'A') {
+            charcode = phrase[i].charCodeAt() + shift;
+            while (charcode > 'Z'.charCodeAt()) {
+                charcode -= 26;
+            }
+        } else {
+            charcode = phrase[i].charCodeAt();
+        }
+        cipher += (String.fromCharCode(charcode)); 
+    }
+    return cipher;
 };
 
 // Do not edit below this line
